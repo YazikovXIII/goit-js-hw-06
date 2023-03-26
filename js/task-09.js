cf764f0c096a57ce1845fdab1c468c2627eca8ed
YazikovXIII <vladzaraza@gmail.com>
2023-03-26 19:21:22 +0300

    123

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const back = document.querySelector('body')
+const colorId = document.querySelector('.color')
+const btnCntrl = document.querySelector('.change-color')
+btnCntrl.addEventListener('click', onClick)
+
+function onClick(event) {
+  back.style.backgroundColor = getRandomHexColor();
+  colorId.textContent = getRandomHexColor()
+}
