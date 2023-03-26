29003aabe1f833cb07b12efc23b49a0e9abbfab3
YazikovXIII <vladzaraza@gmail.com>
2023-03-26 15:10:42 +0300

    123

---
+++ js/task-05.js
@@ -0,0 +1,16 @@
+const inputName = document.querySelector('#name-input');
+const outputName = document.querySelector('#name-output');
+
+inputName.addEventListener('input', onInput)
+
+function onInput(event) {
+    // console.log(event.currentTarget.value);
+    let inputCont = event.currentTarget.value
+    if (inputName.value) {
+        outputName.textContent = inputCont
+        
+        return 
+    }
+
+        outputName.textContent = 'Anonymous'
+}
